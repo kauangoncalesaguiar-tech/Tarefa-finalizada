fafcd39354c8fc26e6b973fc7317e082cff188ef
kauangoncalesaguiar-tech <kauan.goncales.aguiar@escola.pr.gov.br>
2026-03-26 12:42:07 -0300

    quaaaaase pronto

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 // Números 
-let num1 = 324;
-let num2 = 1324;
+    let num1 = 324;
+    let num2 = 1324;
 
 //Soma
-let soma = num1 + num2;
+    let soma = num1 + num2;
 //Subtração
-let sub = num1 - num2;
+    let sub = 50 - 25;
 // Multplicação
-let mult = num1 * num2;
+    let mult = 34 * 87;
 //Divisão
-let div = num1 / num2;
+    let div = 100 / 4;
+
+// Igualdade
+    let verif = 10 == 10 ? 'Igual' : 'Diferente';
+// Diferença
+    let nome1 = 'Styles';
+    let nome2 = 'Peter';
+let verif2 = nome1 != nome2;
+//Menor que 
+    let verif3 = 5 < 10;
+
+//Menor ou igual
+let preço = 50.35;
+let verifpreço = preço > 50;
+
+//Menor ou Igual
+let menIgual = 37 <= 20;
+
+//Maior ou igual
+let idade = 14;
+let verofidade = idade >= 18;
+
+//Atribuição condicional
+let nota = 8
+let verifnota = nota >= 7 ? 'Aprovado' : 'Reprovado';
+
+//Verificação de Maioridade
+let idade2 = 19
+let verofidade2 = idade2 >= 18 ? 'Maior' : 'Menor';
+
+//PlaceHolder
+let nome = 'Kauan';
+let mensagem = 'Olá (nome)'.replace('(nome)', nome);
+
+//Compra
+let produto = 'Arroz'
+let preço = 8.99
+let quantidade = 
+
+let mensagemcompra = 'Você comprou ' + quantidade + 'de ' + produto + 'por ' + preço; 
+
+
+
+
+
+
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(verif);
+console.log(verif2);
+console.log(verif3);
+console.log(verifpreço);
+console.log(menIgual);
+console.log(verofidade);
+console.log(verifnota);
+console.log(verofidade2);
+console.log(mensagem);
+console.log(mensagemcompra);
