c7f10c7a51abc27b956377563c26cd2eae44ec88
kauangoncalesaguiar-tech <kauan.goncales.aguiar@escola.pr.gov.br>
2026-03-30 08:14:04 -0300

    tarefaFinalizada

--- index.js
+++ index.js
@@ -3,33 +3,46 @@
     let num2 = 1324;
 
 //Soma
-    let soma = num1 + num2;
+    let soma = num1 + num2
+    let resultSoma = 'Sua soma deu ' + soma;
+
 //Subtração
-    let sub = 50 - 25;
+    let sub = 50 - 25
+    let resultSub = 'Sua subtração deu ' + sub;
 // Multplicação
-    let mult = 34 * 87;
+    let mult = 34 * 87
+    let ResultMult = 'Sua multiplicação deu ' + mult;
 //Divisão
-    let div = 100 / 4;
+    let div = 100 / 4
+    let ResultDiv = 'Sua divisão deu ' + div
 
 // Igualdade
-    let verif = 10 == 10 ? 'Igual' : 'Diferente';
+    let verif = 10 == 10 ? 'Números iguais' : 'Nomes diferente';
 // Diferença
     let nome1 = 'Styles';
     let nome2 = 'Peter';
-let verif2 = nome1 != nome2;
+let verif2 = nome1 != nome2 ? 'Nomes diferentes' : 'Nomes iguais';
 //Menor que 
-    let verif3 = 5 < 10;
+    let verif3 = 5 < 10 ? 'Menor' : 'Maior';
 
 //Menor ou igual
 let preço = 50.35;
-let verifpreço = preço > 50;
+let verifpreço = preço > 50 ? 'Preço maior' : 'Preço menor';
 
 //Menor ou Igual
-let menIgual = 37 <= 20;
-
+function verifMenorIgual (num3) {
+    if (num3 == 20) {
+        return 'Valor é igual a 20';
+    } else if (num3 < 20) {
+    return 'Valor é menor que 20';
+    } else {
+        return 'Valor é maior que 20';
+    }
+    }
+    
 //Maior ou igual
 let idade = 14;
-let verofidade = idade >= 18;
+let verofidade = idade >= 18 ?  'Maior de Idade' : 'Menor de Idade';
 
 //Atribuição condicional
 let nota = 8
@@ -55,17 +68,18 @@ let mensagemcompra = 'Você comprou ' + quantidade + ' ' + produto + ' por ' + p
 
 
 
-console.log(soma);
-console.log(sub);
-console.log(mult);
-console.log(div);
+console.log(resultSoma);
+console.log(resultSub);
+console.log(ResultMult);
+console.log(ResultDiv);
 console.log(verif);
 console.log(verif2);
 console.log(verif3);
 console.log(verifpreço);
-console.log(menIgual);
+console.log(verifMenorIgual(5));
 console.log(verofidade);
 console.log(verifnota);
 console.log(verofidade2);
 console.log(mensagem);
 console.log(mensagemcompra);
+
